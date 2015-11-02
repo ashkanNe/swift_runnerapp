accf36aa21c5a647b12823f76c23d929ac58fb57
user113876 <user113876@74-80-239-60.ic2net.net>
2015-11-01 16:54:10 -0800

    Minor updates

--- SignUpSecond.swift
+++ SignUpSecond.swift
@@ -2,7 +2,7 @@
 //  SignUpSecond.swift
 //  RunnerApp
 //
-//  Created by Steven Prescott on 9/26/15.
+//  Created by Steven Prescott | Ashkan Neshagaran on 9/26/15.
 //  Copyright (c) 2015 Steven Prescott. All rights reserved.
 //
 
